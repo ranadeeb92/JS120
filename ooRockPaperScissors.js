e5e3410eaa497eb42198385c0fb76cb0b4c436e2
Rana Deeb <ranadeeb@gmail.com>
2020-11-08 18:40:51 -0800

    updated RPS

--- ooRockPaperScissors.js
+++ ooRockPaperScissors.js
@@ -33,8 +33,7 @@
 
 // spock vaporize rock => spock beats rock
 // keeping score unitl one of the player reachs five points
-// 
-
+//
 
 // second step:
 // nouns : rock, scissor, papper, score, lizard, spock, user, computer => player, move, rule, score
@@ -50,14 +49,23 @@
 // methods => verbs
 
 const readline = require("readline-sync");
-const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+const MOVES_TYPE = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
 
 function createComputer() {
   let playerObj = createPlayer();
   let computerObj = {
-    choose() {
-      let randomIndex = Math.floor(Math.random() * choices.length);
-      this.move = choices[randomIndex];
+    choose(moves) {
+      let currentMoves = moves.reduce((result, move) => {
+        let i = 0;
+        while (i <= move.weight) {
+          result.push(move.type);
+          i++;
+        }
+        return result;
+      }, []);
+      console.log(currentMoves);
+      let randomIndex = Math.floor(Math.random() * currentMoves.length);
+      this.move.type = currentMoves[randomIndex];
     },
   };
   return Object.assign(playerObj, computerObj);
@@ -69,12 +77,14 @@ function createHuman() {
     choose() {
       let userInput;
       while (true) {
-        console.log(`Choose one of the following moves: ${choices.join(", ")}`);
+        console.log(
+          `Choose one of the following moves: ${MOVES_TYPE.join(", ")}`
+        );
         userInput = readline.question();
-        if (choices.includes(userInput)) break;
+        if (MOVES_TYPE.includes(userInput)) break;
         console.log("Wrong choice!");
       }
-      this.move = userInput;
+      this.move.type = userInput;
     },
   };
   return Object.assign(palyerObj, humanObj);
@@ -82,18 +92,47 @@ function createHuman() {
 function createPlayer() {
   return {
     // state
-    move: null,
-    score : 0,
-    incrementScore(){
+    move: createMove(),
+    score: 0,
+    incrementScore() {
       this.score += 1;
-    }
+    },
   };
+}
+
+function createMove(type = null) {
+  return {
+    type: type,
+    weight: 0,
+    incrementWeight() {
+      this.weight += 1;
+    },
+    decrementWeight() {
+      if (this.weight - 1 < 0) {
+        this.weight = 0;
+      } else {
+        this.weight -= 1;
+      }
+    },
+  };
+}
+function createMovesArray() {
+  let moves = [];
+  while (moves.length < MOVES_TYPE.length) {
+    MOVES_TYPE.forEach((type) => {
+      let move = createMove(type);
+      moves.push(move);
+    });
+  }
+  return moves;
 }
 
 // next step : Orchestration engine:
 // engine object which orchestrate the objects
 //and where the procedural program flow should be.
 const RPSGame = {
+  movesHistory: [],
+  moves: createMovesArray(),
   human: createHuman(),
   computer: createComputer(),
   displayWelComeMessage() {
@@ -103,9 +142,9 @@ const RPSGame = {
     console.log("Thanks for playing Rock, Paper, Scissors game, Goodbye!");
   },
   displayWinner(winner) {
-    console.log(`Your choice: ${this.human.move}`);
-    console.log(`The computer chose: ${this.computer.move}`);
-    if (winner === "Human"){
+    console.log(`Your choice: ${this.human.move.type}`);
+    console.log(`The computer chose: ${this.computer.move.type}`);
+    if (winner === "Human") {
       console.log("You win!");
     } else if (winner === "Computer") {
       console.log("Computer win!");
@@ -113,86 +152,134 @@ const RPSGame = {
       console.log(winner);
     }
   },
-  compareChoices(){
+  compareChoices() {
     if (
-      (this.human.move === "Paper" && (this.computer.move === "Rock" || this.computer.move === "Spock")) ||
-      (this.human.move === "Rock" && (this.computer.move === "Scissors" || this.computer.move === "Lizard")) ||
-      (this.human.move === "Scissors" && (this.computer.move === "Paper" || this.computer.move === "Lizard"))||
-      (this.human.move === "Lizard" && (this.computer.move === "Spock" || this.computer.move === "Paper" )) ||
-      (this.human.move === "Spock" && (this.computer.move === "Rock" || this.computer.move === "Scissors") )
+      (this.human.move.type === "Paper" &&
+        (this.computer.move.type === "Rock" ||
+          this.computer.move.type === "Spock")) ||
+      (this.human.move.type === "Rock" &&
+        (this.computer.move.type === "Scissors" ||
+          this.computer.move.type === "Lizard")) ||
+      (this.human.move.type === "Scissors" &&
+        (this.computer.move.type === "Paper" ||
+          this.computer.move.type === "Lizard")) ||
+      (this.human.move.type === "Lizard" &&
+        (this.computer.move.type === "Spock" ||
+          this.computer.move.type === "Paper")) ||
+      (this.human.move.type === "Spock" &&
+        (this.computer.move.type === "Rock" ||
+          this.computer.move.type === "Scissors"))
     ) {
       return "Human";
     } else if (
-      (this.computer.move === "Paper" && (this.human.move === "Rock" || this.human.move === "Spock")) ||
-      (this.computer.move === "Rock" && (this.human.move === "Scissors" || this.human.move === "Lizard")) ||
-      (this.computer.move === "Scissors" && (this.human.move === "Paper" || this.human.move === "Lizard"))||
-      (this.computer.move === "Lizard" && (this.human.move === "Spock" || this.human.move === "Paper" )) ||
-      (this.computer.move === "Spock" && (this.human.move === "Rock" || this.human.move === "Scissors") )
+      (this.computer.move.type === "Paper" &&
+        (this.human.move.type === "Rock" ||
+          this.human.move.type === "Spock")) ||
+      (this.computer.move.type === "Rock" &&
+        (this.human.move.type === "Scissors" ||
+          this.human.move.type === "Lizard")) ||
+      (this.computer.move.type === "Scissors" &&
+        (this.human.move.type === "Paper" ||
+          this.human.move.type === "Lizard")) ||
+      (this.computer.move.type === "Lizard" &&
+        (this.human.move.type === "Spock" ||
+          this.human.move.type === "Paper")) ||
+      (this.computer.move.type === "Spock" &&
+        (this.human.move.type === "Rock" ||
+          this.human.move.type === "Scissors"))
     ) {
       return "Computer";
     } else {
       return "It Is A Tie!";
     }
   },
-  changeScore(winner){
-    if (winner === "Human"){
+  changeScore(winner) {
+    if (winner === "Human") {
       this.human.score += 1;
     } else if (winner === "Computer") {
       this.computer.score += 1;
-    } 
+    }
   },
-  playMatch(){
-      this.clear();
-      this.displayScores();
-      this.human.choose();
-      this.computer.choose();
-      let winner = this.compareChoices();
-      this.displayWinner(winner);
-      this.changeScore(winner);
+  playMatch() {
+    this.clear();
+    this.displayScores();
+    this.human.choose();
+    this.computer.choose(this.moves);
+    let winner = this.compareChoices();
+    this.changeScore(winner);
+    this.storeGameMoves(winner);
+    this.displayWinner(winner);
+    this.analyzeGameMoves();
+  },
+  analyzeGameMoves() {
+    // let lastHands = this.movesHistory[this.movesHistory.length - 1];
+    // let computerMove = lastHands[0];
+    // let move = this.moves.find((move) => move.type === computerMove);
+    this.moves.forEach((move) => {
+      if (this.isWinningMove(move.type)) {
+        move.incrementWeight();
+      } else {
+        move.decrementWeight();
+      }
+    });
+  },
+  isWinningMove(move) {
+    let choosingTimes = this.movesHistory.filter((ele) => ele[0] === move)
+      .length;
+    let winningTimes = this.movesHistory.filter(
+      (ele) => ele[0] === move && ele[2] === "Computer"
+    ).length;
+    let winningRate = Math.floor((winningTimes / choosingTimes) * 100);
+    return winningRate > 60;
   },
   palyAgain() {
     console.log("Would you like to play again? Enter(Yes/No)");
     let answer = readline.question().toLowerCase();
     return answer[0] === "y";
   },
-  displayGameWinner(){
-    if(this.human.score === 5){
+  displayGameWinner() {
+    if (this.human.score === 5) {
       console.log("You won the Game!");
-    }else{
+    } else {
       console.log("Computer won the Game!");
     }
   },
-  displayScores(){
-    console.log(`Your score: ${this.human.score}, Computer score: ${this.computer.score}`);
+  displayScores() {
+    console.log(
+      `Your score: ${this.human.score}, Computer score: ${this.computer.score}`
+    );
+  },
+  storeGameMoves(winner) {
+    this.movesHistory.push([
+      this.computer.move.type,
+      this.human.move.type,
+      winner,
+    ]);
   },
   clear() {
     console.clear();
   },
-  reset(){
+  reset() {
     this.human.score = 0;
     this.computer.score = 0;
-    this.human.move = null;
-    this.computer.move = null;
   },
   play() {
     this.displayWelComeMessage();
-    while(true){
+    while (true) {
       this.reset();
       this.clear();
-      while(true){
+      while (true) {
         this.playMatch();
-        if(this.human.score === 5 || this.computer.score === 5) break;
+        if (this.human.score === 5 || this.computer.score === 5) break;
         readline.question("Enter any letter to continue..");
       }
       this.clear();
       this.displayScores();
       this.displayGameWinner();
-      if(!this.palyAgain()) break;
+      if (!this.palyAgain()) break;
     }
     this.displayGoodbyeMessage();
-  }
+  },
 };
 
 RPSGame.play();
-
-
